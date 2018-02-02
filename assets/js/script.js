f515965e903b268af71c3f82c5cca2020a7d4524
Leonardo Antunes <antunesleo4@gmail.com>
2018-02-01 23:21:41 -0200

    Implementing best code pratices

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,75 +1,64 @@
-//Global variables
-var repos = []; //hold a list of repos of the user
-var commits = []; // hold a list of commits of the user
-var head; //head element of the page, used by jsonp requests
-var userName; //github username, used by a lot of functions
+var repos = [];
+var commits = [];
+var head;
+var userName;
 
 window.onload = function () {
-	var buttonValidate = document.getElementById("buttonValidate");
+  var buttonValidate = document.getElementById("buttonValidate");
 	var buttonRandom = document.getElementById("buttonRandom");
 
 	head = document.getElementsByTagName("head")[0];
 	userName = document.getElementById("inputUser");
-	buttonValidate.addEventListener("click", triggerRest);
-	buttonRandom.addEventListener("click", randomCommit);
+	buttonValidate.addEventListener("click", showACommitRandonly);
 	previewHandler();
 }
 
-/*
- * This function receives the response from the github rest API (JSONP)
- * for each repository, the function will create a new scrip JSONP element
- * for retrieve all commit per repository
- */
-function getRepos(response) {
 
-	var data = response.data; //Getting data of the response provided by the github api containg all repositories
+function showACommitRandonly() {
+  var url = "https://api.github.com/users/{}/repos?callback=getRepositories".format(userName.value);
+  
+  console.log(url);
+
+	var scriptElement = document.createElement("script");
+	scriptElement.setAttribute("src", url);
+	scriptElement.setAttribute("id","jsonpRepo");
+	head.appendChild(scriptElement);
+}
+
+function getRepositories(response) {
+
+  var data = response.data; 
+  try {
+    if (data.message === 'Not Found') alert('not found')
+  } catch (err) {
+    
+  }
 	for (i = 0; i < data.length; i++) {
-		repos.push(data[i].name); //add the repo to the array
+		repos.push(data[i].name);
 
-		//constructing the url, passing user name and repository name
-		var urli = "https://api.github.com/repos/" + userName.value + "/" + data[i].name + "/commits?callback=getCommits";
-		var scriptElementi = document.createElement("script"); //creates a new dom element Script
-		scriptElementi.setAttribute("src", urli); //passing the url for the element
-		scriptElementi.setAttribute("id","jsonpCommits" + data[i].name); //setting an id for the script element jsonpCommit + repositoryName
-		head.appendChild(scriptElementi); //append the script tag for the head element
-	}
+    var urli = "https://api.github.com/repos/{}/{}/commits?callback=getCommits".format(userName.value, data[i].name);
+		var scriptElementi = document.createElement("script");
+		scriptElementi.setAttribute("src", urli);
+		scriptElementi.setAttribute("id","jsonpCommits" + data[i].name);
+    head.appendChild(scriptElementi);
+
+    randomCommit();
+  }
 }
 
-
-/*
- * This is the trigger function for all rest request stuff
- *
- */
-function triggerRest() {
-	var url = "https://api.github.com/users/" + userName.value + "/repos?callback=getRepos"; //url to get all repos passing user name
-	var scriptElement = document.createElement("script"); //creating a jsonp script element
-	scriptElement.setAttribute("src", url); //passing de url for the script element
-	scriptElement.setAttribute("id","jsonpRepo"); //set and id for the element
-	head.appendChild(scriptElement); //adding the script element to the head element
-	document.getElementById("buttonRandom").style.display = "inline";
-}
-
-/*
- * this function gets the response from the Github API rest
- * the response contains all the commit on the repository
- * each commit message is saved on the array commits
- */
 function getCommits(response) {
 
-	var data = response.data;
+  var data = response.data;
 
 	for (i = 0; i < data.length -1; i++) {
 		commits.push(data[i].commit.message);
-	}
+  }
+
 }
 
-/*
- * This function get a random commit message stored on the array and
- * add in a div for the user
- */
 function randomCommit() {
 	var commitDiv = document.getElementById("commit");
-	while (commitDiv.firstChild) { //if exists a commit message being showed, will be removed
+	while (commitDiv.firstChild) {
 		commitDiv.removeChild(commitDiv.firstChild);
 	}
 	var div = document.createElement("div");
@@ -81,10 +70,6 @@ function randomCommit() {
 	var context = document.getElementById("canvasCommit").getContext("2d");
 }
 
-/*
- * Functions to draw the random squares below the navbar
- *
- */
 function previewHandler() {
 	var canvas = document.getElementById("canvasCommit");
 	var context = canvas.getContext("2d");
@@ -117,3 +102,10 @@ function randomColorHexa() {
 	var colors = ["#D6E685","#8CC665","#44A340","#1E6823"];
 	return colors[Math.floor(Math.random() * 4)];
 }
+
+String.prototype.format = function () {
+  var i = 0, args = arguments;
+  return this.replace(/{}/g, function () {
+    return typeof args[i] != 'undefined' ? args[i++] : '';
+  });
+};
